b24f8ce981e59b067a0d6171ee24747f2ba3979b
Andrija Gajic <andrija.gajic152@gmail.com>
2023-09-18 21:20:20 +0200

    Frontend

--- Flask/project.py
+++ Flask/project.py
@@ -1,53 +1,36 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 import os
-import zipfile
-from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__)
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('index.html', error=error)
 
-@app.route('/upload', methods=['POST'])
-def receive_data():
-    data_type = request.form['data_type']
+app.config['UPLOAD_FOLDER'] = 'files' 
 
-    if data_type == 'D':
-        # Receive the zip archive containing the folder
-        folder_zip = request.files['file']
-        folder_name = folder_zip.filename
+@app.route('/')
+def home():
+    return render_template('login.html')
 
-        # Ensure a unique filename
-        base_directory = "multivitamin"  # Replace with your desired directory
-        folder_path = os.path.join(base_directory, folder_name)
-        while os.path.exists(folder_path):
-            folder_name, ext = os.path.splitext(folder_name)
-            folder_name += "_copy"
-            folder_path = os.path.join(base_directory, folder_name)
+@app.route('/upload', methods=["POST"])
+def upload_file():
+    if "file" in request.files:
+        file = request.files["file"]
+        if file.filename != '':
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "File uploaded successfully"
 
-        folder_zip.save(folder_path)
+    return "File not uploaded"
 
-        # Extract the zip archive to the specified directory
-        with zipfile.ZipFile(folder_path, 'r') as zipf:
-            zipf.extractall(base_directory)
-
-        os.remove(folder_path)
-        return jsonify({"message": f"Folder '{folder_name}' received and saved successfully."})
-
-    elif data_type == 'F':
-        # Receive a single file
-        file = request.files['file']
-        file_name = file.filename
-
-        # Ensure a unique filename
-        base_directory = "multivitamin"  # Replace with your desired directory
-        file_path = os.path.join(base_directory, file_name)
-        while os.path.exists(file_path):
-            file_name, ext = os.path.splitext(file_name)
-            file_name += "_copy"
-            file_path = os.path.join(base_directory, file_name)
-
-        file.save(file_path)
-        return jsonify({"message": f"File '{file_name}' received and saved successfully."})
-
-    else:
-        return jsonify({"error": "Invalid data type received."})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=12345)
+    app.run(debug=True)
